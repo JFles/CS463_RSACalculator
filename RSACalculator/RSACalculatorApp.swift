bf6e7899f285f691f28659b5bc0d1f76a7b0a01e
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-11-23 20:32:05 -0600

    Initial Commit

---
+++ RSACalculator/RSACalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSACalculatorApp.swift
+//  RSACalculator
+//
+//  Created by Jeremy Fleshman on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct RSACalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
