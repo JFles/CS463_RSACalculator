7ce9f647a499ac1fae6cbd9899fca626ca72c117
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-11-23 21:44:17 -0600

    Started building out basic UI

--- RSACalculator/Views/ContentView.swift
+++ RSACalculator/Views/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var p = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                PublicValueEntry(label: "Prime p:", placeholder: "Enter a prime number", input: $p)
+
+
+                Spacer()
+            }
+            .navigationBarTitle(Text("RSA Calculator"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PublicValueEntry: View {
+    var label: String
+    var placeholder: String
+    @Binding var input: String
+
+    var body: some View {
+        HStack {
+            Text(label)
+            TextField(placeholder, text: $input)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }.padding()
     }
 }
